35e21dcf27d97da58744e0910e6d973488671cb2
Robin Dunlop <rdunlop@thoughtworks.com>
2013-01-08 22:50:54 -0800

    don't compile assets on heroku

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module ReviewSite
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # To support Heroku
+    config.assets.initialize_on_precompile = false
   end
 end

2786c7db0afec67834f804210460d0c3a9b834c7
Aleksandar Serafimoski <aserafimoski@gmail.com>
2013-01-14 19:48:57 -0600

    Fixing the build

--- db/migrate/20130111010550_change_column_name_in_jc.rb
+++ db/migrate/20130111010550_change_column_name_in_jc.rb
@@ -1,6 +1,5 @@
 class ChangeColumnNameInJc < ActiveRecord::Migration
   def change
     rename_column :junior_consultants, :coach, :coach_id
-    change_column :junior_consultants, :coach_id, :integer
   end
 end

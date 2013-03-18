4c7d8e0d221c94ecd21b3a83ab25b558a55718af
nphollon <nhollon@thoughtworks.com>
2013-03-18 16:26:26 -0500

    #203 Reviewees may now invite their own reviewers.

--- spec/requests/home_page_spec.rb
+++ spec/requests/home_page_spec.rb
@@ -1,18 +1,28 @@
 require 'spec_helper'
 
 describe "Home page" do
-  let (:admin) { FactoryGirl.create :admin_user }
   let! (:jc) { FactoryGirl.create :junior_consultant }
   let! (:review) { FactoryGirl.create :review, junior_consultant: jc }
 
   subject { page }
 
-  before do
-    sign_in admin
+  describe "as admin" do
+    let (:admin) { FactoryGirl.create :admin_user }
+    before { sign_in admin }
+
+    it "can navigate to the reviewer invitation page" do
+      click_link 'Invite Reviewer'
+      page.should have_selector('h1', text: 'Invite Reviewer')
+    end
   end
 
-  it "can navigate to the reviewer invitation page" do
-    click_link 'Invite Reviewer'
-    page.should have_selector('h1', text: 'Invite Reviewer')
+  describe "as JC" do
+    let (:jc_user) { FactoryGirl.create :user, email: jc.email }
+    before { sign_in jc_user }
+
+    it "can navigate to the reviewer invitation page" do
+      click_link 'Invite Reviewer'
+      page.should have_selector('h1', text: 'Invite Reviewer')
+    end
   end
 end

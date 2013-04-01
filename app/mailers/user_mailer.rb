4bac72f0b08f4ba2de92c1e7b948660391c35e74
nphollon <nhollon@thoughtworks.com>
2013-04-01 11:47:17 -0500

    Remove username from mailer's 'To:' field

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,17 +2,17 @@ class UserMailer < ActionMailer::Base
 
   def self_registration_confirmation(user)
     @user = user
-    mail(:to => "#{user.name} <#{user.email}>", :subject => "Thank you for registering on the ReviewSite")
+    mail(:to => "<#{user.email}>", :subject => "Thank you for registering on the ReviewSite")
   end
 
   def admin_registration_confirmation(user)
     @user = user
-    mail(:to => "#{user.name} <#{user.email}>", :subject => "You were registered on the ReviewSite")
+    mail(:to => "<#{user.email}>", :subject => "You were registered on the ReviewSite")
   end
 
   def password_reset(user)
     @user = user
-    mail(:to => "#{user.name} <#{user.email}>", :subject => "Reset password for the ReviewSite")
+    mail(:to => "<#{user.email}>", :subject => "Reset password for the ReviewSite")
   end
 
   def new_feedback_notification(feedback)
@@ -20,7 +20,7 @@ class UserMailer < ActionMailer::Base
     @review = feedback.review
     @reviewer = feedback.user
     @reviewee = @review.junior_consultant
-    mail(:to => "#{@reviewee.name} <#{@reviewee.email}>", :subject => "You have new feedback")
+    mail(:to => "<#{@reviewee.email}>", :subject => "You have new feedback")
   end
 
   def review_invitation(review, email, message)

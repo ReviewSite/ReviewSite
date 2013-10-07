e8ece1ecd11e1d9fe6da69ab597fda41722f6ebd
Svetlana Ivanova <sivanova@thoughtworks.com>
2013-10-07 10:30:31 -0500

    Move logic from views to helpers

--- app/helpers/junior_consultants_helper.rb
+++ app/helpers/junior_consultants_helper.rb
@@ -1,2 +1,9 @@
 module JuniorConsultantsHelper
+  def alphabetically_sorted_user_names
+    User.all.map { |user| [user.name, user.id]}.sort
+  end
+
+  def reviewing_group_list
+    ReviewingGroup.all.map { |rg| [rg.name, rg.id] }
+  end
 end

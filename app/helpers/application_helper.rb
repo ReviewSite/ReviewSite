c082dfca32f6ac10f57e081f4e454a4646930ab6
Svetlana Ivanova <sivanova@thoughtworks.com>
2013-10-10 22:31:37 -0500

    Move alphabetically order logic to application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,19 +8,7 @@ module ApplicationHelper
     end
   end
 
-  def alphabetically_sorted_reviewing_groups
-    ReviewingGroup.all.map { |rg| [rg.name, rg.id] }.sort
-  end
-
-  def users_list
-    User.all.map { |user| [user.name, user.id] } 
-  end
-
-  def alphabetically_sorted_user_names
-    User.all.map { |user| [user.name, user.id]}.sort
-  end
-
-  def alphabetically_sort_junior_consultant_names
-    JuniorConsultant.all.map { |jc| [jc.name, jc.id] }.sort
+  def format_all_for_select(model)
+    model.all.map { |model_object| [model_object.name, model_object.id] }.sort
   end
 end

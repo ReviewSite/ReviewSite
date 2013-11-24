78aa0a6c2588e7661fa838bdb54be8abdabdc4ae
Robin Dunlop <rdunlop@thoughtworks.com>
2013-11-24 18:28:28 +0000

    refactor review#summary page to have different contents depending on the type of review (new vs old)

--- app/models/review.rb
+++ app/models/review.rb
@@ -18,6 +18,32 @@ class Review < ActiveRecord::Base
     self.new_review_format = true if new_record?
   end
 
+  def headings
+    if self.new_review_format
+      ["Role Competence","Consulting Skills","Teamwork", "Contritubions"]
+    else
+      ["Tech","Client", "Ownership", "Leadership", "Teamwork", "Attitude", "Professionalism", "Organizational", "Innovative"]
+    end
+  end
+  def headers
+    if self.new_review_format
+      ["role_competence_went_well", "role_competence_to_be_improved", "role_competence_action_items",
+        "consulting_skills_went_well", "consulting_skills_to_be_improved", "consulting_skills_action_items",
+        "teamwork_went_well", "teamwork_to_be_improved", "teamwork_action_items",
+        "contritubions_went_well", "contritubions_to_be_improved", "contritubions_action_items"]
+    else
+      ["tech_exceeded", "tech_met", "tech_improve",
+        "client_exceeded", "client_met", "client_improve",
+        "ownership_exceeded", "ownership_met", "ownership_improve",
+        "leadership_exceeded", "leadership_met", "leadership_improve",
+        "teamwork_exceeded", "teamwork_met", "teamwork_improve",
+        "attitude_exceeded", "attitude_met", "attitude_improve",
+        "professionalism_exceeded", "professionalism_met", "professionalism_improve",
+        "organizational_exceeded", "organizational_met", "organizational_improve",
+        "innovative_exceeded", "innovative_met", "innovative_improve"]
+    end
+  end
+
   def to_s
     "#{junior_consultant} - #{review_type}"
   end

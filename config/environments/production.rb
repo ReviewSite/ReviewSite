e9949f4b762b6087a5312c390bd4718c8776c696
Robin Dunlop <rdunlop@thoughtworks.com>
2013-11-25 13:29:58 -0600

    update production use of exceptionNotifier (which was upgraded in the 'dev' configuration, but missed the 'prod' one)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,10 @@
 ReviewSite::Application.configure do
-  config.middleware.use ExceptionNotifier,
-      :email_prefix => "[ReviewSite Error] ",
-      :sender_address => %{<do-not-reply@thoughtworks.org>},
-      :exception_recipients => %w{nhollon@thoughtworks.com cahn@thoughtworks.com rdunlop@thoughtworks.com}
+  config.middleware.use ExceptionNotification::Rack,
+    :email => {
+        :email_prefix => "[ReviewSite Error] ",
+        :sender_address => %{<do-not-reply@thoughtworks.org>},
+        :exception_recipients => %w{nhollon@thoughtworks.com cahn@thoughtworks.com rdunlop@thoughtworks.com}
+    }
 
   # Settings specified here will take precedence over those in config/application.rb
 

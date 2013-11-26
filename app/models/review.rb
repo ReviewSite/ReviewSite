14e0de4b4798e6ccf7d2344d8496208dacbb3044
Robin Dunlop <rdunlop@thoughtworks.com>
2013-11-26 15:09:59 +0000

    remove the 'action_items' from the new reviews (only in the UI, not in the DB), also update the review#summary pages to fetch all their fields from the review instead of hard-coded (ie: Comments)

--- app/models/review.rb
+++ app/models/review.rb
@@ -62,45 +62,46 @@ class Review < ActiveRecord::Base
                                            "<p>Simple new ideas that made lives easier on the project or clients/ unique innovative solutions in or outside of projects</p>")
 
     # new-style questions
-    questions["Role Competence"] = Question.new("Role Competence", "Role Competence", ["role_competence_went_well", "role_competence_to_be_improved", "role_competence_action_items"],
+    questions["Role Competence"] = Question.new("Role Competence", "Role Competence", ["role_competence_went_well", "role_competence_to_be_improved"],
                                                 "<p> <b>Understanding:</b> Tech stack, domain, trust to onboard others <br />" +
                                                 "<b>Ownership of concept:</b> go-to person for specific questions, accountability, leads discussions, confidence<br />" + 
                                                 "<b>Learnings:</b> Eager to learn new tools/apps/languages, ability to apply learnings, innovative, side projects, self directed learnings<br />" +
                                                 "<b>Effective collaborator:</b> Good knowledge transfer, effective pair, knows shortcut keys, drives/navagates/rotates pairs, desk checks<br /></p>",
                                                 "role_competence_scale")
-    questions["Consulting Skills"] = Question.new("Consulting Skills", "Consulting Skills", ["consulting_skills_went_well", "consulting_skills_to_be_improved", "consulting_skills_action_items"],
+    questions["Consulting Skills"] = Question.new("Consulting Skills", "Consulting Skills", ["consulting_skills_went_well", "consulting_skills_to_be_improved"],
                                                   "<p><b>Customer Commitment:</b> understand what the client really needs, understand deadlines and why they are important, building relationships with the client, being able to influence client de    velopers to use technologies<br />" +
                                                   "<b>Communication skils:</b> Asks questions, professionally offers concerns and alternative solutions, ability to influence, push back when needed<br />" +
                                                   "<b>Delivery:</b> Stretches to deliver if need be, understands need to deliver on time<br />" + 
                                                   "<b>Aware of Perceptions:</b> professional client facing presence, good understanding of business needs vs. personal wants<br /></p>",
                                                   "consulting_skills_scale")
 
-    questions["Teamwork"] = Question.new("Teamwork", "Teamwork", ["teamwork_went_well", "teamwork_to_be_improved", "teamwork_action_items"],
+    questions["Teamwork"] = Question.new("Teamwork", "Teamwork", ["teamwork_went_well", "teamwork_to_be_improved"],
                                          "<p><b>Relationship building:</b> Working with team members, conflict resolution, people want to work with you again<br />" +
                                          "<b>Feedback:</b> Ability to deliver and ask for feedback, reacts well to feedback<br />" +
                                          "<b>Attitude:</b> Approachable, enthusiastic, helpful, respectful, will work outside of 40 hours to help project if needed<br />" +
                                          "<b>Professionalism:</b> Professional client-facing presence, punctual, turns in time sheets, acceptable attendance and visability to team<br /></p>",
                                          "teamwork_scale")
 
-    questions["Contributions"] = Question.new("Contributions", "Contributions", ["contributions_went_well", "contributions_to_be_improved", "contributions_action_items"],
+    questions["Contributions"] = Question.new("Contributions", "Contributions", ["contributions_went_well", "contributions_to_be_improved"],
                                               "<p><b>Knowledge Sharing:</b> L&Ls, HOD, Away Day, JC Continuing Learning Sessions, hallway chats<br />" +
                                               "<b>Social Resposibility:</b> Awareness, involvement, contribution<br />" +
                                               "<b>TW sponsored events: ex:</b> SIP, Black Girls Code, Rails Girls, Black @ TW, WNB<br /></p>",
                                               "contributions_scale")
 
+    # BOTH
+    questions["Comments"] = Question.new("Comments", "General Comments", ["comments"],
+                                         "<p>Anything not covered above</p>")
     questions
   end
 
   # these MUST match keys in the questions block above
   def headings
+    # NOTE: "Comments" is shared between both sets
     if self.new_review_format
-      ["Role Competence","Consulting Skills","Teamwork", "Contributions"]
+      ["Role Competence","Consulting Skills","Teamwork", "Contributions", "Comments"]
     else
-      ["Tech","Client", "Ownership", "Leadership", "OldTeamwork", "Attitude", "Professionalism", "Organizational", "Innovative"]
+      ["Tech","Client", "Ownership", "Leadership", "OldTeamwork", "Attitude", "Professionalism", "Organizational", "Innovative", "Comments"]
     end
-  end
-  def headers
-    headings.collect {|heading| questions[heading].fields}.flatten
   end
 
   def heading_title(heading)

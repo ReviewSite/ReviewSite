78f697b259db94dc83fcdea2597fb6894d05a8e0
Valerie Roske <vroske@gmail.com>
2014-05-13 23:59:49 +0000

    #297 & #298: Valerie: added fields for users to reviewing group form and implemented autocomplete

--- app/models/reviewing_group.rb
+++ app/models/reviewing_group.rb
@@ -1,5 +1,6 @@
 class ReviewingGroup < ActiveRecord::Base
-  attr_accessible :name, :users_attributes
+  attr_accessible :name, :user_tokens
+  attr_reader :user_tokens
 
   has_many :reviewing_group_members, :dependent => :destroy
   has_many :users, :dependent => :nullify
@@ -7,7 +8,12 @@ class ReviewingGroup < ActiveRecord::Base
   validates :name, presence: true
 
   accepts_nested_attributes_for :reviewing_group_members
-  accepts_nested_attributes_for :users
+  #accepts_nested_attributes_for :users
+
+  def user_tokens=(ids)
+    self.user_ids = ids.split(",")
+  end
+
 
   def members
     self.users.map { |m| m.to_s}.sort.join(', ')

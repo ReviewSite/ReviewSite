d04708af3970bc08cf3575c77fb61d8f43daae52
Valerie Roske <vroske@gmail.com>
2014-06-12 21:59:28 +0000

    Valerie: #tech: removed unused files

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ ReviewSite::Application.routes.draw do
 
   resources :reviewing_groups, except: [:show]
 
-  resources :admin, only: [:index]
-
   resources :junior_consultants, only: [:index]
 
   resources :reviews do
@@ -11,17 +9,21 @@ ReviewSite::Application.routes.draw do
         get :summary
         get :send_email
     end
+
     resources :feedbacks, :except => [:index] do
       member do
         put :submit
         put :unsubmit
         post :send_reminder
       end
+
       collection do
         get :additional
       end
     end
+
     resources :self_assessments, except: [:index, :show]
+
     resources :invitations, only: [:new, :create, :destroy] do
       member do
         post :send_reminder
@@ -30,7 +32,9 @@ ReviewSite::Application.routes.draw do
   end
 
   root :to => 'welcome#index'
+
   resources :sessions, only: [:new, :create, :destroy]
+
   resources :password_resets, only: [:new, :create, :edit]
 
   match "/auth/:provider/callback" => "sessions#callback"
@@ -48,71 +52,10 @@ ReviewSite::Application.routes.draw do
 
   resources :users do
     get :feedbacks, :on => :member
-    # get :autocomplete_coach_name, :on => :collection
-  end
-
-  scope :users, :controller => 'users' do
-    get 'autocomplete_user_name'
   end
 
   if ENV['OKTA-TEST-MODE']
     match '/set_temp_okta', to: 'sessions#set_temp_okta', via: :post
   end
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end

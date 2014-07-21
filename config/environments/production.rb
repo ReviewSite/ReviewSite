c5cfede6a6a517890ee78f2611054f7485ca4a89
Valerie Roske <varoske@thoughtworks.com>
2014-07-21 03:56:33 +0000

    tech: change error email and provide check for nil review date in upcoming review method

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ ReviewSite::Application.configure do
     :email => {
         :email_prefix => "[ReviewSite Error] ",
         :sender_address => %{<do-not-reply@thoughtworks.org>},
-        :exception_recipients => %w{nhollon@thoughtworks.com cahn@thoughtworks.com rdunlop@thoughtworks.com}
+        :exception_recipients => %w{varoske@thoughtworks.com}
     }
 
   # Settings specified here will take precedence over those in config/application.rb

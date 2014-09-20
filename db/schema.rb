f56daaf66aa2a47212b4c08a6a670bdb1073a90c
Alex Jablonski <ajablons@thoughtworks.com>
2014-09-20 19:47:57 +0000

    Fix schema version from incorrect merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140917154751) do
+ActiveRecord::Schema.define(:version => 20140918143921) do
 
   create_table "associate_consultants", :force => true do |t|
     t.datetime "created_at",         :null => false

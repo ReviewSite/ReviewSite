dbb742c9b0b5f317f15366369654e72ecb3ad1cc
Valerie Roske <vroske@gmail.com>
2014-09-21 22:29:01 -0500

    remove unnecessary eager loading

--- app/helpers/feedbacks_helper.rb
+++ app/helpers/feedbacks_helper.rb
@@ -8,7 +8,7 @@ module FeedbacksHelper
   def open_requests(user)
 
     invitations = []
-    Invitation.includes(:review).all.each do |invitation|
+    Invitation.all.each do |invitation|
       if invitation.sent_to?(current_user) and !invitation.feedback
         invitations << invitation
       end

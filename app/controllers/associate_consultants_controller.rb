3e90e089100f0bb5bccb7493d9bb758391c04a30
Valerie Roske <varoske@thoughtworks.com>
2014-09-23 21:06:20 +0000

    graduated acs don't appear in autocomplete

--- app/controllers/associate_consultants_controller.rb
+++ app/controllers/associate_consultants_controller.rb
@@ -5,7 +5,7 @@ def index
 
   name = "%#{params[:q]}%"
 
-  @associate_consultants = AssociateConsultant.joins(:user).where("users.name ILIKE ?", name )
+  @associate_consultants = AssociateConsultant.joins(:user).where("users.name ILIKE ?", name).where(graduated: [false, nil])
 
   respond_to do |format|
     format.json { render :json => @associate_consultants.map{ |ac| {:id => ac.id, :name => ac.user.name} } }

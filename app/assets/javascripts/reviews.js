92a0bac6e59524b5f8fca27213a2c840bd2e0e24
Alex Jablonski <ajablons@thoughtworks.com>
2014-10-02 16:49:39 -0500

    #302 - Auto-populate feedback deadline with 7 days before review date

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -22,4 +22,15 @@ jQuery(function () {
 
   $( document ).tooltip();
 
+  $("#review_review_date").change(function() {
+    var date = new Date( $("#review_review_date").val() );
+    var dayInMs = 24 * 60 * 60 * 1000;
+    var weekAgo = new Date( date - 7 * dayInMs);
+    var weekAgoDateString =
+      weekAgo.getUTCFullYear() + "-" +
+      (weekAgo.getUTCMonth() + 1) + "-" + // JS months are 0-indexed :(
+      weekAgo.getUTCDate();
+
+    $("#review_feedback_deadline").val( weekAgoDateString );
+  });
 });

f7661dc6be50ec22260dada2f39b62b29b2fcf3b
Valerie Roske <varoske@thoughtworks.com>
2014-10-23 22:49:01 +0000

    fix date display issues

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 ReviewSite::Application.initialize!
 
-Date::DATE_FORMATS.merge!(
-  :short_date => "%b. %d, %Y"
-)
+Date::DATE_FORMATS[:short_date] = "%b. %d, %Y"
+Time::DATE_FORMATS[:short_date] = "%b. %d, %Y"

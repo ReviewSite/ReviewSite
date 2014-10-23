dea825a32f5976ef4ec3f2ecd722065403a9e89c
Brandon Smith <brsmith@thoughtworks.com>
2014-10-23 22:46:05 +0000

    Password Reset now checks for blank email addresses

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,15 +5,21 @@ class PasswordResetsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    user.request_password_reset if user
-    redirect_to signin_path, notice: "Password reset email has been sent."
+    if params[:email].empty?
+      redirect_to new_password_reset_path,
+        alert: "Please include an email address."
+    else
+      user = User.find_by_email(params[:email])
+      user.request_password_reset if user
+      redirect_to signin_path, notice: "Password reset email has been sent."
+    end
   end
 
   def edit
     @user = User.find_by_password_reset_token!(params[:id])
     if @user.password_reset_sent_at < 2.hours.ago
-      redirect_to new_password_reset_path, :alert => "Password reset has expired."
+      redirect_to new_password_reset_path,
+        :alert => "Password reset has expired."
     else
       first_time_sign_in @user
     end

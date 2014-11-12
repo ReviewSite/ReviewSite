051c8d0b4b32ec995f22b2ac03d338c76a4a302d
Alex Jablonski <ajablons@thoughtworks.com>
2014-11-12 17:23:04 -0600

    Refactor to use "subject" in tests

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -1,21 +1,21 @@
-require 'spec_helper'
-require 'cancan/matchers'
+require "spec_helper"
+require "cancan/matchers"
 describe Ability do
   describe "Admin" do
     before do
       @admin_user = FactoryGirl.create(:admin_user)
       @ability = Ability.new(@admin_user)
     end
-    subject{@ability}
-    it{ should be_able_to(:manage, Review) }
-    it{ should be_able_to(:manage, ReviewingGroup) }
-    it{ should be_able_to(:manage, AssociateConsultant) }
-    it{ should be_able_to(:manage, User) }
-    it{ should be_able_to(:submit, Feedback) }
-    it{ should be_able_to(:unsubmit, Feedback) }
-    it{ should be_able_to(:summary, Review) }
-    it{ should be_able_to(:read, Feedback) }
-    it{ should be_able_to(:update, User)}
+    subject { @ability }
+    it { should be_able_to(:manage, Review) }
+    it { should be_able_to(:manage, ReviewingGroup) }
+    it { should be_able_to(:manage, AssociateConsultant) }
+    it { should be_able_to(:manage, User) }
+    it { should be_able_to(:submit, Feedback) }
+    it { should be_able_to(:unsubmit, Feedback) }
+    it { should be_able_to(:summary, Review) }
+    it { should be_able_to(:read, Feedback) }
+    it { should be_able_to(:update, User) }
   end
 
   describe "User" do
@@ -23,17 +23,17 @@ describe Ability do
       @user = FactoryGirl.create(:user)
       @ability = Ability.new(@user)
     end
-    subject{@ability}
-    it{ should_not be_able_to(:manage, Review) }
-    it{ should_not be_able_to(:manage, ReviewingGroup) }
-    it{ should_not be_able_to(:manage, AssociateConsultant) }
-    it{ should_not be_able_to(:manage, User) }
-    it{ should be_able_to(:manage, SelfAssessment) }
-    it{ should_not be_able_to(:submit, Feedback) }
-    it{ should_not be_able_to(:unsubmit, Feedback) }
-    it{ should be_able_to(:summary, Review) }
-    it{ should be_able_to(:read, Feedback) }
-    it{ should be_able_to(:update, User)}
+    subject { @ability }
+    it { should_not be_able_to(:manage, Review) }
+    it { should_not be_able_to(:manage, ReviewingGroup) }
+    it { should_not be_able_to(:manage, AssociateConsultant) }
+    it { should_not be_able_to(:manage, User) }
+    it { should be_able_to(:manage, SelfAssessment) }
+    it { should_not be_able_to(:submit, Feedback) }
+    it { should_not be_able_to(:unsubmit, Feedback) }
+    it { should be_able_to(:summary, Review) }
+    it { should be_able_to(:read, Feedback) }
+    it { should be_able_to(:update, User) }
   end
 
   describe "User with review" do
@@ -52,29 +52,32 @@ describe Ability do
         @ability = Ability.new(@user)
       end
 
+      subject { @ability }
+
       it "should be able to send reminders on feedback for own review" do
-        @ability.should be_able_to(:send_reminder, @feedback)
+        should be_able_to(:send_reminder, @feedback)
       end
 
-      it "should not be able to view, update, or destroy others' unsubmitted feedback" do
-        @ability.should_not be_able_to(:read, @feedback)
-        @ability.should_not be_able_to(:update, @feedback)
-        @ability.should_not be_able_to(:destroy, @feedback)
+      it "should not be able to view, update, or destroy "\
+          "others' unsubmitted feedback" do
+        should_not be_able_to(:read, @feedback)
+        should_not be_able_to(:update, @feedback)
+        should_not be_able_to(:destroy, @feedback)
       end
 
       it "should only be able to manage self assessments for own reviews" do
-        @ability.should be_able_to(:manage, @review.self_assessments.new)
         other_review = FactoryGirl.create(:review)
-        @ability.should_not be_able_to(:manage, other_review.self_assessments.new)
+        should be_able_to(:manage, @review.self_assessments.new)
+        should_not be_able_to(:manage, other_review.self_assessments.new)
       end
 
       it "should allow users to create additional feedback for own reviews" do
-        @ability.should be_able_to(:create, @review.feedbacks.build)
+        should be_able_to(:create, @review.feedbacks.build)
       end
 
       it "should be able to read submitted feedback for own review" do
         @feedback.submitted = true
-        @ability.should be_able_to(:read, @feedback)
+        should be_able_to(:read, @feedback)
       end
     end
 
@@ -83,24 +86,32 @@ describe Ability do
         @giver_ability = Ability.new(@feedback.user)
       end
 
-      it "should allow users to create feedback for reviews they are invited to" do
+      subject { @giver_ability }
+
+      it "should allow users to create feedback "\
+          "for reviews they are invited to" do
         invited_user = FactoryGirl.create(:user)
-        invitation = FactoryGirl.create(:invitation, review: @review, email: invited_user.email)
+        FactoryGirl.create(
+          :invitation,
+          review: @review,
+          email: invited_user.email)
         invited_user_ability = Ability.new(invited_user)
         invited_user_ability.should be_able_to(:create, @review.feedbacks.build)
       end
 
-      it "should be able to view, update, and destroy own unsubmitted feedback" do
-        @giver_ability.should be_able_to(:read, @feedback)
-        @giver_ability.should be_able_to(:update, @feedback)
-        @giver_ability.should be_able_to(:destroy, @feedback)
+      it "should be able to view, update, and destroy "\
+          "own unsubmitted feedback" do
+        should be_able_to(:read, @feedback)
+        should be_able_to(:update, @feedback)
+        should be_able_to(:destroy, @feedback)
       end
 
-      it "should be able to view, but not update, or destroy submitted feedback" do
+      it "should be able to view, but not update, or destroy "\
+          "submitted feedback" do
         @feedback.submitted = true
-        @giver_ability.should be_able_to(:read, @feedback)
-        @giver_ability.should_not be_able_to(:update, @feedback)
-        @giver_ability.should_not be_able_to(:destroy, @feedback)
+        should be_able_to(:read, @feedback)
+        should_not be_able_to(:update, @feedback)
+        should_not be_able_to(:destroy, @feedback)
       end
     end
 
@@ -110,26 +121,30 @@ describe Ability do
         @admin_ability = Ability.new(admin_user)
       end
 
+      subject { @admin_ability }
+
       it "should not be able to create additional feedback for user review" do
-        @admin_ability.should_not be_able_to(:additional, @review.feedbacks.build)
-        @admin_ability.should_not be_able_to(:create, @review.feedbacks.build)
+        should_not be_able_to(:additional, @review.feedbacks.build)
+        should_not be_able_to(:create, @review.feedbacks.build)
       end
 
-      it "should not be able to view, update, or destroy others' unsubmitted feedback" do
-        @admin_ability.should_not be_able_to(:read, @feedback)
-        @admin_ability.should_not be_able_to(:update, @feedback)
-        @admin_ability.should_not be_able_to(:destroy, @feedback)
+      it "should not be able to view, update, or destroy "\
+          "others' unsubmitted feedback" do
+        should_not be_able_to(:read, @feedback)
+        should_not be_able_to(:update, @feedback)
+        should_not be_able_to(:destroy, @feedback)
       end
 
-      it "should be able to view, but not update or destroy submitted feedback" do
+      it "should be able to view, but not update or destroy "\
+          "submitted feedback" do
         @feedback.submitted = true
-        @admin_ability.should be_able_to(:read, @feedback)
-        @admin_ability.should_not be_able_to(:update, @feedback)
-        @admin_ability.should_not be_able_to(:destroy, @feedback)
+        should be_able_to(:read, @feedback)
+        should_not be_able_to(:update, @feedback)
+        should_not be_able_to(:destroy, @feedback)
       end
 
       it "should be able to send reminders on feedback" do
-        @admin_ability.should be_able_to(:send_reminder, @feedback)
+        should be_able_to(:send_reminder, @feedback)
       end
     end
 
@@ -147,7 +162,8 @@ describe Ability do
     end
 
     describe "as reviewing group member" do
-      it "should be able to read submitted feedback as reviewing group member" do
+      it "should be able to read submitted feedback "\
+          "as reviewing group member" do
         @feedback.submitted = true
         reviewing_group_member = FactoryGirl.create(:user)
         @ac.reviewing_group.users << reviewing_group_member
@@ -163,15 +179,19 @@ describe Ability do
         @other_user_ability = Ability.new(other_user)
       end
 
-      it "should not be able to send reminders on feedback for others' reviews" do
-        @other_user_ability.should_not be_able_to(:send_reminder, @feedback)
+      subject { @other_user_ability }
+
+      it "should not be able to send reminders on feedback "\
+          "for others' reviews" do
+        should_not be_able_to(:send_reminder, @feedback)
       end
 
-      it "should not be able to view, but not update, or destroy submitted feedback" do
+      it "should not be able to view, but not update, or destroy "\
+          "submitted feedback" do
         @feedback.submitted = true
-        @other_user_ability.should_not be_able_to(:read, @feedback)
-        @other_user_ability.should_not be_able_to(:update, @feedback)
-        @other_user_ability.should_not be_able_to(:destroy, @feedback)
+        should_not be_able_to(:read, @feedback)
+        should_not be_able_to(:update, @feedback)
+        should_not be_able_to(:destroy, @feedback)
       end
     end
   end

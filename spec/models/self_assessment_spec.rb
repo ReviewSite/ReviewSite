8f5138337eadfa1d482158c3fb5cb7349e3a1176
Brandon Smith <brsmith@thoughtworks.com>
2015-01-06 11:33:14 -0600

    Clean up tests for self assessments using let and a before block

--- spec/models/self_assessment_spec.rb
+++ spec/models/self_assessment_spec.rb
@@ -1,30 +1,26 @@
 require 'spec_helper'
 
 describe SelfAssessment do
-  it "requires an ac" do
-    self_assessment = SelfAssessment.new
-    self_assessment.review = FactoryGirl.create(:review)
-    self_assessment.valid?.should == false
+  let(:self_assessment) { SelfAssessment.new }
 
+  before do
+    self_assessment.review = FactoryGirl.create(:review)
     self_assessment.associate_consultant = FactoryGirl.create(:associate_consultant)
+  end
+
+  it "requires an ac" do
+    self_assessment.valid?.should == false
     self_assessment.response = "stuff"
     self_assessment.valid?.should == true
   end
 
   it "requires a review" do
-    self_assessment = SelfAssessment.new
-    self_assessment.associate_consultant = FactoryGirl.create(:associate_consultant)
     self_assessment.valid?.should == false
-
-    self_assessment.review = FactoryGirl.create(:review)
     self_assessment.response = "stuff"
     self_assessment.valid?.should == true
   end
 
   it "requires response text" do
-    self_assessment = SelfAssessment.new
-    self_assessment.associate_consultant = FactoryGirl.create(:associate_consultant)
-    self_assessment.review = FactoryGirl.create(:review)
     self_assessment.valid?.should == false
   end
 end

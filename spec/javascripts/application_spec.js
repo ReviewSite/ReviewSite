ba6e0f3bfe05c00d93d198972e463216590b18f2
Valerie Roske <varoske@thoughtworks.com>
2015-01-18 16:44:35 +0000

    #43: add sample jasmine jquery test

--- spec/javascripts/application_spec.js
+++ spec/javascripts/application_spec.js
@@ -1,6 +1,22 @@
-// sample test to make sure jasmine is set up correctly
-describe('Foo', function() {
+// sample jasmine test
+describe("Foo", function() {
   it("does something", function() {
     expect(1 + 1).toBe(2);
+  });
+});
+
+// sample jasmine-jquery test
+describe("Bar",  function() {
+  it("does a thing", function() {
+    // you can keep fixtures in a separate file and use the
+    // loadFixtures() function instead
+    setFixtures('<button id="derp"></button>');
+
+    var spyEvent = spyOnEvent("#derp", "click");
+
+    $("#derp").click();
+
+    expect("click").toHaveBeenTriggeredOn("#derp");
+    expect(spyEvent).toHaveBeenTriggered();
   });
 });

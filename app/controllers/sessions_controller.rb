b60cac10409645e8f02d80d35230ec2fef3d5e31
Valerie Roske <varoske@thoughtworks.com>
2015-02-05 22:02:09 -0600

    address houndci comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   skip_authorization_check
 
   def callback
-    session[:userinfo] = request.env['omniauth.auth'].uid
+    session[:userinfo] = request.env["omniauth.auth"].uid
     redirect_back_or(root_url)
   end
 
@@ -14,12 +14,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-   user = User.find_by_email(params[:session][:email].downcase)
-   if user && user.authenticate(params[:session][:password])
-     first_time_sign_in user
-   else
-     flash.now[:error] = 'Invalid email/password combination'
-     render 'new'
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      first_time_sign_in user
+    else
+      flash.now[:error] = "Invalid email/password combination"
+      render "new"
     end
   end
 

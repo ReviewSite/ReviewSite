1dded42de23ba204817083bc3e4e61447b3ee475
Allie Quintano <aquintan@thoughtworks.com>
2015-05-06 17:39:40 -0500

    runs tests in random order -allie/mark

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,6 +27,8 @@ RSpec.configure do |config|
   # config.mock_with :flexmock
   # config.mock_with :rr
 
+  config.order = :random
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 

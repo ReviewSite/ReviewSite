6d708f2c9f474b9d1c8e36080350efd4e732dcc2
bteng22 <bteng22@gmail.com>
2015-07-07 15:55:26 -0500

    move General Comments to top of review.headings stack and set accordion default to it

--- app/models/review/heading.rb
+++ app/models/review/heading.rb
@@ -1,4 +1,4 @@
 class Review::Heading
-	NEW_FORMAT = %w{Role\ Competence Consulting\ Skills Teamwork Contributions Comments}
+	NEW_FORMAT = %w{Comments Role\ Competence Consulting\ Skills Teamwork Contributions}
 	OLD_FORMAT = %w{Tech Client Ownership Leadership OldTeamwork Attitude Professionalism Organizational Innovative Comments}
 end

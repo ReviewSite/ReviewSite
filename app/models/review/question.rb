ab33e3b0cb7590fbbc6bedff7055fb4ad0816f51
bteng22 <bteng22@gmail.com>
2015-07-13 14:56:03 -0500

    additional changes for removing priority dropdown/category scales

--- app/models/review/question.rb
+++ app/models/review/question.rb
@@ -1,11 +1,10 @@
 class Review::Question
   attr_accessor :heading, :title, :fields, :description, :scale_field
 
-  def initialize(heading, title, fields, description, scale_field = nil)
+  def initialize(heading, title, fields, description)
     @heading = heading
     @title = title
     @fields = fields
     @description = description
-    @scale_field = scale_field
   end
 end

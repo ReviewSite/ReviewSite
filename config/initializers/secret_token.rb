e2fcc2dff85a920474d19d87247fa27a15c5c8b9
bteng22 <bteng22@gmail.com>
2015-08-05 12:18:03 -0500

    #116 - security improvements with figaro gem by extracting sensitive information to env variables

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ReviewSite::Application.config.secret_token = 'ce6de5cf6ab565296342ab430b7db186ad49405984140b85e6ebebcd37fd803e70d90c6074160492b6c0f25e913dd7a42824a324ef564edd0dca991c9824c620'
+ReviewSite::Application.config.secret_token = ENV.fetch('RAILS_SECRET_TOKEN')

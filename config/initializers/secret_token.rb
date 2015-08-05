adef07982eb33182732beedb1410ccff47f48b7e
bteng22 <bteng22@gmail.com>
2015-08-05 15:07:50 -0500

    #116 - dont use fetch for ENV

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ReviewSite::Application.config.secret_token = ENV.fetch('RAILS_SECRET_TOKEN')
+ReviewSite::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']

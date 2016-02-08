864b6159519167a819c8640295ab2364608e680d
Brandon Smith <brsmith@thoughtworks.com>
2016-02-08 21:46:10 +0000

    #175 - [Brandon] Add tests to check side nav when review date has passed.

--- spec/requests/sidenav_pages_spec.rb
+++ spec/requests/sidenav_pages_spec.rb
@@ -27,6 +27,22 @@ describe "Side Navbar" do
 
         page.should have_selector("a", text: "Submit Self-Assessment")
       end
+
+      it "should show ask-for-feedback button" do
+        @review = create(:review, associate_consultant: ac,
+          review_date: Date.today - 5.years, review_type: "6-Month", feedback_deadline: Date.today - 6.years)
+        visit review_path(@review)
+
+        page.should have_selector("a", text: "Ask For Feedback")
+      end
+
+      it "should show record external feedback button" do
+        @review = create(:review, associate_consultant: ac,
+          review_date: Date.today - 5.years, review_type: "6-Month", feedback_deadline: Date.today - 6.years)
+        visit review_path(@review)
+
+        page.should have_selector("a", text: "Record External Feedback")
+      end
     end
   end
 end

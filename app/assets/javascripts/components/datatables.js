9ae0b0c7292389be6c9036fc5cf309fa8b206bf5
Aidan Obley <aobley@thoughtworks.com>
2016-05-10 10:54:10 -0400

    Aidan/Lauren #197 Update logic for displaying icons on Track Feedback Progress page

--- app/assets/javascripts/components/datatables.js
+++ app/assets/javascripts/components/datatables.js
@@ -36,7 +36,7 @@ var feedbackRequests = function() {
 var feedbackSeenByAC = function() {
   $("#feedback-received").DataTable({
     columnDefs: [
-      { orderable: false, targets: [5,6,7] }
+      { orderable: false, targets: [5,6] }
     ],
     paging: false,
     lengthChange: false,
